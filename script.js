f6fb5f0e4a70f5a4c4377e9e0074f16621bf1f3d
Sachin007060 <sachinbaskandi20@gmail.com>
2024-04-29 10:16:27 +0530

    added script js file

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = ()=>{console.log("hello js..");}
+a();
+
+let square = n=> console.log(n*n);
+square(5);
+
+let sum = (n1, n2)=>console.log(`sum of ${n1} and ${n2} = ${n1+n2}`);
+sum(4,5);
+
